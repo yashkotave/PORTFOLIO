5eb4d239ee584b98b1340a422fb6a8353bfdb7b0
yashkotave <yashkotave1007@gmail.com>
2026-07-13 16:18:03 +0530

    fix: update resume URL to the correct link

--- client/src/config/data.js
+++ client/src/config/data.js
@@ -26,7 +26,7 @@ export const personalInfo = {
   location: "Bhopal, India",
   email: "yashkotave1007@gmail.com",
   phone: "+91 9165324126",
-  resumeUrl: "https://drive.google.com/file/d/1JmrL6rWsQ3tNRaLTUaQZkdKQl4e9PttG/view?usp=sharing",
+  resumeUrl: "https://drive.google.com/file/d/15zI7y3XFDjroXp66tUEia1iGRfMZU6ic/view?usp=sharing",
 
   // Social Media Links
   socials: {
